bda19009c39fb113c0196c92f0709b8a49447ed7
Success <ikhinobelesuccess@gmail.com>
2022-07-22 00:22:48 +0100

    added tailwind css installation files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Kanban</h1>
+      <h1 className="text-3xl font-bold underline">Kanban</h1>
     </div>
   );
 }
