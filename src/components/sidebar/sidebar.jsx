4f0a84e9cd9f40e677b54e88d2800cbc7a55649b
Success <ikhinobelesuccess@gmail.com>
2022-08-03 22:59:22 +0100

    updates

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/images/logo.png";
 import MenuIcon from "../../assets/images/menuicons.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LightMode from "../../assets/images/lightmode.png";
 import DarkMode from "../../assets/images/darkmode.png";
+import ToggleOn from "../../assets/images/toggledarkmode.png";
+import ToggleOff from "../../assets/images/togglelightmode.png";
 
 const Sidebar = () => {
   //created an array containing the menus in the sidebar.
   const sidebarMenu = ["Platform Launch", "Marketing Plan", "Roadmap"];
+
+  // creating a dark state so I can toggle
+  const { dark, setDark } = useState(false);
+  console.log(dark);
+
   return (
     <div className="bg-white dark:bg-[#2B2C37] h-screen">
       <div className="w-full p-4 flex gap-3 items-center ">
@@ -38,10 +45,24 @@ const Sidebar = () => {
       </div>
 
       <div className="h-1/3 p-4 w-full">
-        <div className="flex w-full rounded h-10 p-2 justify-center items-center gap-1 bg-[#F4F7FD] dark:bg-[#20212C]">
-          <img src={LightMode} alt="light mode" />
-          <FontAwesomeIcon icon="fa-solid fa-toggle-off" />
-          <img src={DarkMode} alt="dark mode" />
+        <div className="flex w-full rounded h-10 p-2 justify-center items-center gap-2 bg-[#F4F7FD] dark:bg-[#20212C]">
+          <img
+            className="cursor-pointer"
+            onClick={() => setDark(false)}
+            src={LightMode}
+            alt="light mode"
+          />
+          {dark ? (
+            <img src={ToggleOn} alt="dark mode on" />
+          ) : (
+            <img src={ToggleOff} alt="dark mode off" />
+          )}
+          <img
+            className="cursor-pointer"
+            onClick={() => setDark(true)}
+            src={DarkMode}
+            alt="dark mode"
+          />
         </div>
         <p>Hide sidebar</p>
       </div>
