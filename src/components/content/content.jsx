66a1be72325f12a8a9ae0216fddbdba6be6f6eea
Success <ikhinobelesuccess@gmail.com>
2022-08-09 00:27:40 +0100

    updates

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Content = () => {
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="h-screen">
       <div className="flex w-full h-20 p-4 justify-between items-center bg-white dark:bg-[#2B2C37] border-l-2 ">
@@ -9,14 +11,19 @@ const Content = () => {
         </h3>
 
         <div>
-          <button className="bg-[#635FC7] text-white p-4 rounded-full border-none w-40 h-12 flex items-center justify-center font-jakarta">
+          <button
+            className={`${
+              disabled ? "opacity-70" : "opacity-100"
+            } bg-[#635FC7] text-white p-4 rounded-full border-none w-40 h-12 flex items-center justify-center font-jakarta`}
+            disabled={true}
+          >
             +Add New Task
           </button>
         </div>
       </div>
 
       <div className="w-full flex flex-col items-center justify-center">
-        <h4 className="mt-80 text-[#828FA3] text-lg font-bold font-jakarta">
+        <h4 className="mt-64 text-[#828FA3] text-lg font-bold font-jakarta">
           This board is empty. Create a new column to get started.
         </h4>
         <button className="bg-[#635FC7] text-white p-4 rounded-full border-none w-48 mt-4 h-12 flex items-center justify-center font-jakarta">
