ab1fd9ced692cad0ca795e6c71c589912cac025b
Success <ikhinobelesuccess@gmail.com>
2022-08-21 00:20:57 +0100

    updates

--- src/components/modals/newTask.jsx
+++ src/components/modals/newTask.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import CloseBtn from "../../assets/images/close.png";
 
 const NewTask = (props) => {
   const { newTaskModal, setNewTaskModal } = props;
 
   return (
-    <div className="fixed h-full w-full inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center " onClick={() => setNewTaskModal(false)}>
-      <div className="w-80 h-80 bg-white rounded-lg">
+    <div
+      className="fixed h-full w-full inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center "
+      onClick={() => setNewTaskModal(false)}
+    >
+      <div
+        className="w-80 h-80 bg-white rounded-lg relative"
+        onClick={(e) => e.stopPropagation()}
+      >
         <h2>Add New Task</h2>
+
+        <img
+          src={CloseBtn}
+          alt="close button"
+          className="w-4 h-4 cursor-pointer absolute top-4 right-4"
+          onClick={() => setNewTaskModal(false)}
+        />
       </div>
     </div>
   );
